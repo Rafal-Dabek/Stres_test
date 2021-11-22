325c7d5ab986e15c489622395e0638ae8a05795e
Adam <barcelona1999.aw@gmail/com>
2021-11-22 15:10:54 +0100

    Update

--- script.js
+++ script.js
@@ -1,4 +1,212 @@
+function done2()
+{
+  var zadaniowe = 0;
+  var okołoemocjonalne = 0;
 
+  //okołoemocjonalne
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("1_"+i.toString()).checked == true)
+    {
+      okołoemocjonalne += parseInt(document.getElementById("1_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("2_"+i.toString()).checked == true)
+    {
+      okołoemocjonalne += parseInt(document.getElementById("2_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("3_"+i.toString()).checked == true)
+    {
+      okołoemocjonalne += parseInt(document.getElementById("3_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("5_"+i.toString()).checked == true)
+    {
+      okołoemocjonalne += parseInt(document.getElementById("5_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("6_"+i.toString()).checked == true)
+    {
+      okołoemocjonalne += parseInt(document.getElementById("6_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("9_"+i.toString()).checked == true)
+    {
+      okołoemocjonalne += parseInt(document.getElementById("9_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("11_"+i.toString()).checked == true)
+    {
+      okołoemocjonalne += parseInt(document.getElementById("11_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("12_"+i.toString()).checked == true)
+    {
+      okołoemocjonalne += parseInt(document.getElementById("12_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("15_"+i.toString()).checked == true)
+    {
+      okołoemocjonalne += parseInt(document.getElementById("15_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("19_"+i.toString()).checked == true)
+    {
+      okołoemocjonalne += parseInt(document.getElementById("19_"+i.toString()).value)
+    }
+  }
+
+  //zadaniowe
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("4_"+i.toString()).checked == true)
+    {
+      zadaniowe += parseInt(document.getElementById("4_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("7_"+i.toString()).checked == true)
+    {
+      zadaniowe += parseInt(document.getElementById("7_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("8_"+i.toString()).checked == true)
+    {
+      zadaniowe += parseInt(document.getElementById("8_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("10_"+i.toString()).checked == true)
+    {
+      zadaniowe += parseInt(document.getElementById("10_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("13_"+i.toString()).checked == true)
+    {
+      zadaniowe += parseInt(document.getElementById("13_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("14_"+i.toString()).checked == true)
+    {
+      zadaniowe += parseInt(document.getElementById("14_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("16_"+i.toString()).checked == true)
+    {
+      zadaniowe += parseInt(document.getElementById("16_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("17_"+i.toString()).checked == true)
+    {
+      zadaniowe += parseInt(document.getElementById("17_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("18_"+i.toString()).checked == true)
+    {
+      zadaniowe += parseInt(document.getElementById("18_"+i.toString()).value)
+    }
+  }
+
+  for(var i=1; i<6; i++)
+  {
+    if(document.getElementById("20_"+i.toString()).checked == true)
+    {
+      zadaniowe += parseInt(document.getElementById("20_"+i.toString()).value)
+    }
+  }
+
+
+  var ele = document.getElementsByTagName('input');
+                
+  var ele = document.getElementsByTagName('input');
+  var d=[];
+  for(i = 0; i < ele.length; i++)
+  {
+    if(ele[i].type="radio") {
+                      
+      if(ele[i].checked)
+      {
+
+        if(ele[i].value!=null)d+= ele[i].value ;
+                        
+      }
+                          
+
+    }
+              
+  }
+
+
+  var p=Array.from(d);
+
+  if((d.length)!=20)
+  {
+
+      alert("Nie wszytkie odpowiedzi są zaznaczone!")
+            
+  }
+  else
+  {
+    if(!isNaN(document.getElementById("disp").innerHTML))
+    {
+        document.getElementById("disp").innerHTML += "Wynik okołoemocjonalny: " + okołoemocjonalne + "<br>" +
+        "Wynik zadaniowy: " + zadaniowe;
+    }
+  }
+
+}
 
 
 
